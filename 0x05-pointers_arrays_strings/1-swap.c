e983aa05e16c1fd652433f173eb3fe0525d31e02
Abderahmon <abdulrahmon.ola122@gmail.com>
2022-09-19 20:35:07 -0700

    Task2-5

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,9 @@
  * swap_in - Swapping the values of two integers
  * @a: ponter to first value
  * @b: pointer to second value
+ * Return: returns nothing
  */
+
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,13 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
-void wap_int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
