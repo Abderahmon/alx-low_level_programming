700073d28202caf587dff7ee1e7d131ff2a0ebe4
Abderahmon <abdulrahmon.ola122@gmail.com>
2022-10-13 18:43:05 -0700

    Task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _3-FUNCTION_LIKE_MACRO_
-#define _3-FUNCTION_LIKE_MACRO_
+#ifndef _3-FUNCTION_LIKE_MACRO_H
+#define _3-FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < (0) ? ((x) - (1)) : (x))
 
